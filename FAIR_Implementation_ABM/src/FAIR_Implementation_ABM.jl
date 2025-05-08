ea2cb8528c1e42088e5690ad2ffd832edcaacbdb
isagimon <Isabella.gimon.leos@gmail.com>
2025-05-08 13:51:04 -0400

    Edits: 05/08

--- FAIR_Implementation_ABM/src/FAIR_Implementation_ABM.jl
+++
@@ -1,5 +0,0 @@
-module FAIR_Implementation_ABM
-
-greet() = print("Hello World!")
-
-end # module FAIR_Implementation_ABM
--- src/FAIR_Implementation_ABM.jl
+++
@@ -1,9 +0,0 @@
-__precompile__(false)
-
-module FAIR_Implementation_ABM
-
-include("../Agents.jl")
-include("../Environment_and_Movement.jl")
-# Add additional includes here if you have more logic in other .jl files
-
-end
